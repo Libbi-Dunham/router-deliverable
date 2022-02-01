5c276ea0f4a2b708d285cb9ab66bc415fac39322
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-01 14:15:26 -0800

    made another function wrote out detail page

--- src/Views/CharacterDetail/CharacterDetail.js
+++ src/Views/CharacterDetail/CharacterDetail.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getCharacterById } from '../../services/character';
 
-export default function CharacterDetail({ Characters }) {
+export default function CharacterDetail() {
+  const { id } = useParams();
+  const [character, setCharacter] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const characterData = await getCharacterById(id);
+      setCharacter(characterData);
+      setLoading(false);
+    };
+    fetchData();
+  }, [id]);
+
+  if (loading) return <p> Loading </p>;
   return (
     <div className="detail">
-      {Characters.map((char) => (
-        <p key={char.id}>
-          {char.Characters}
-          <p> Nickname: {char.nickname}</p>
-          <p> Occupation: {char.occupation}</p>
-        </p>
-      ))}
+      <div>
+        <Character {...character} />
+      </div>
     </div>
   );
 }
--- src/services/character.js
+++ src/services/character.js
@@ -3,3 +3,9 @@ export async function getCharacters() {
   const charactersData = await response.json();
   return charactersData;
 }
+
+export async function getCharacterById(id) {
+  const response = await fetch(`https://breakingbadapi.com/api/characters/${id}`);
+  const characterData = await response.json();
+  return characterData;
+}
