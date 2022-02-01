ae3c574d217ed6ed3e3b0f2b3079a66a79daba58
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-01 15:12:57 -0800

    added in useHistory

--- src/Component/Character/Character.js
+++ src/Component/Character/Character.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-export default function Character({ character }) {
+export default function Character({ character, handleSubmit }) {
   return (
-    <div className="character">
-      <h2> {character.name} </h2>
-      <img src={character.img}></img>
-      <h2> {character.nickname} </h2>
-      <h2> {character.occupation} </h2>
-    </div>
+    <>
+      <div className="character">
+        <h2> {character.name} </h2>
+        <img src={character.img}></img>
+        <h2> {character.nickname} </h2>
+        <h2> {character.occupation} </h2>
+      </div>
+      <div>
+        <button className="button" onClick={handleSubmit}>
+          Go Home
+        </button>
+      </div>
+    </>
   );
 }
--- src/Views/CharacterDetail/CharacterDetail.js
+++ src/Views/CharacterDetail/CharacterDetail.js
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getCharacterById } from '../../services/character';
 import Character from '../../Component/Character/Character';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function CharacterDetail() {
   const { id } = useParams();
   const [character, setCharacter] = useState([]);
   const [loading, setLoading] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,11 +20,16 @@ export default function CharacterDetail() {
     fetchData();
   }, [id]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    history.push('/');
+  };
+
   if (loading) return <p> Loading </p>;
   return (
     <div className="detail">
       <div>
-        <Character {...{ character }} />
+        <Character {...{ character }} handleSubmit={handleSubmit} />
       </div>
     </div>
   );
